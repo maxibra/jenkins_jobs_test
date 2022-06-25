a7105ebf94a5f7709336b7c9d8a3c804a7b20fda
maxibra <max.braver@gmail.com>
2022-06-26 02:02:50 +0300

    Update haskellGit_playground.groovy

--- jobs/haskellGit_playground.groovy
+++ jobs/haskellGit_playground.groovy
@@ -1,4 +1,12 @@
 pipelineJob('example') {
     displayName('Job DSL Example Project')
     description('My first job')
+    deliveryPipelineConfiguration('qa', 'integration-tests')
+    logRotator {
+        numToKeep(10)
+        artifactNumToKeep(1)
+    }
+    throttleConcurrentBuilds {
+        categmaxTotalories(1)
+    }
 }
